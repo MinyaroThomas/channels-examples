ec1c92f2c8ab416a5d104010468753df5309d9b0
Minyaro Thomas <162447320+MinyaroThomas@users.noreply.github.com>
2025-04-20 15:21:02 +0300

    Update models.py

--- multichat/chat/models.py
+++ multichat/chat/models.py
@@ -1,24 +1,13 @@
-from django.db import models
+from django.contrib.auth.models import User
 
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+    participants = models.ManyToManyField(User)
+    is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class Room(models.Model):
-    """
-    A room for people to chat in.
-    """
-
-    # Room title
-    title = models.CharField(max_length=255)
-
-    # If only "staff" users are allowed (is_staff on django's User)
-    staff_only = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-
-    @property
-    def group_name(self):
-        """
-        Returns the Channels Group name that sockets should subscribe to to get sent
-        messages as they are generated.
-        """
-        return "room-%s" % self.id
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
