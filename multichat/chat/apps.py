ac9e1157879ef958d3bdf2855748f6e3f567e13a
Minyaro Thomas <162447320+MinyaroThomas@users.noreply.github.com>
2025-04-20 15:35:26 +0300

    Update apps.py

--- multichat/chat/apps.py
+++ multichat/chat/apps.py
@@ -1,5 +1,17 @@
-from django.apps import AppConfig
+INSTALLED_APPS = [
+    # ...
+    'channels',
+    'chat',
+    'allauth',
+    'allauth.account',
+]
 
-
-class ChatConfig(AppConfig):
-    name = 'chat'
+ASGI_APPLICATION = 'chat_app.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
