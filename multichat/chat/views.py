afe48de299fba22dbec9e5cfc88b221826d41ef2
Minyaro Thomas <162447320+MinyaroThomas@users.noreply.github.com>
2025-04-20 15:24:29 +0300

    Update views.py

--- multichat/chat/views.py
+++ multichat/chat/views.py
@@ -1,18 +1,7 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Room
-
+from django.shortcuts import render
 
 @login_required
-def index(request):
-    """
-    Root page view. This is essentially a single-page app, if you ignore the
-    login and admin parts.
-    """
-    # Get a list of rooms, ordered alphabetically
-    rooms = Room.objects.order_by("title")
-
-    # Render that in the index template
-    return render(request, "index.html", {
-        "rooms": rooms,
-    })
+def room(request, room_id):
+    room = ChatRoom.objects.get(id=room_id)
+    return render(request, 'chat/room.html', {'room': room})
